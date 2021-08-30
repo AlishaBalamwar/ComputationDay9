3f843e9bfe0adc51f58061826d2f781c49421117
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-30 13:33:40 +0530

    checks employee present or not using method

--- src/com/bridgelabz/computation/EmployeeWageComputation.java
+++ src/com/bridgelabz/computation/EmployeeWageComputation.java
@@ -3,5 +3,18 @@ package com.bridgelabz.computation;
 public class EmployeeWageComputation {
 	public static void main(String[] args ) {
    System.out.println("Welcome to Employee Wage Computation");
+   checkEmpPresentorAbsent();
    }
+	static void checkEmpPresentorAbsent() {
+		int present =1;
+		 int empcheck =(int) Math.floor(Math.random()*10)%2;
+		if(empcheck==present) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
+	}
+	
 }
+
