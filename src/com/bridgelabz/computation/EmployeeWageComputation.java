76883961ac092602423c5fdfd1215e2ff9e23291
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-30 13:46:08 +0530

    calculates daily wage of employee

--- src/com/bridgelabz/computation/EmployeeWageComputation.java
+++ src/com/bridgelabz/computation/EmployeeWageComputation.java
@@ -4,15 +4,21 @@ public class EmployeeWageComputation {
 	public static void main(String[] args ) {
    System.out.println("Welcome to Employee Wage Computation");
    checkEmpPresentorAbsent();
+	
    }
 	static void checkEmpPresentorAbsent() {
-		int present =1;
+		 int present =1;
+		 int empWagePerHour=20;
+			int fullDayHour=8;
 		 int empcheck =(int) Math.floor(Math.random()*10)%2;
 		if(empcheck==present) {
 			System.out.println("Employee is present");
+			int dailyWage=empWagePerHour*fullDayHour;
+			System.out.println("Daily wage of employee is "+dailyWage);
 		}
 		else {
 			System.out.println("Employee is absent");
+			System.out.println("As employee is absent so his daily wage is 0");
 		}
 	}
 	
